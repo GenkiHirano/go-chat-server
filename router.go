815fbcdad363a601df36d10ac0ae4860f1efad84
GenkiHirano <genki86web@gmail.com>
2022-09-04 14:50:40 +0900

    feat: Run関数実装

--- router.go
+++ router.go
@@ -1,5 +1,16 @@
 package chat
 
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
 func Run() {
-	// チャット機能を実装予定
+	r := gin.Default()
+	m := melody.New()
+
+	r.Static("/static", "./view/static")
+	r.LoadHTMLGlob("view/*.html")
+
+	// GinでGet実装予定
 }
